4f24dd45d418f69008b265ec3a6901250282d2c3
shibanandakatual <shibanandakatual@gmail.com>
2024-03-19 12:09:00 +0530

    some changes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Modernize Free</title>
+  <title>FRESHMART</title>
   <link rel="shortcut icon" type="image/png" href="{{asset('assets/img/logos/favicon.png')}}" />
   <link rel="stylesheet" href="{{asset('assets/css/styles.min.css')}}" />
 </head>
@@ -23,7 +23,6 @@
                 <a href="./index.html" class="text-nowrap logo-img text-center d-block py-3 w-100">
                   <img src="{{asset('assets/img/logos/dark-logo.svg')}}" width="180" alt="">
                 </a>
-                <p class="text-center">Your Social Campaigns</p>
                 <form method="POST" action="{{route('register')}}">
                    @csrf
                   <div class="mb-3">
@@ -34,9 +33,13 @@
                     <label for="exampleInputEmail1" class="form-label">Email Address</label>
                     <input type="email"  name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                   </div>
-                  <div class="mb-4">
+                  <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Password</label>
                     <input type="password"  name="password" class="form-control" id="exampleInputPassword1">
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
+                    <input type="password"  name="Confirmpassword" class="form-control" id="exampleInputPassword2">
                   </div>
                   <input type="submit" class="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2" value="Sign Up">
                   <!-- <a href="./index.html" class="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2">Sign Up</a> -->
--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::get('/profile', [ProfileController::class, 'update'])->name('profile.update');
 require __DIR__.'/auth.php';
